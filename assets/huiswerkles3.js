d56a1c420f2cb13b5a896e1361fe858053991fff
ridd0053 <ridd0053@hz.nl>
2019-09-16 15:01:41 +0200

    message

---
+++ assets/huiswerkles3.js
@@ -0,0 +1,15 @@
+//Opdracht 1:
+let number = 5;
+const evenOrOdd = number % 2;
+if(evenOrOdd === 1){
+    console.log('odd');
+}else{
+    console.log('even');
+};
+//opdracht 2:
+const message = 'Programming is not so cool';
+console.log(message.substring(0, 15) + message.substring(19, 26));
+
+//opdracht 3:
+console.log(1400 === 'Ik woon in Naboo');
+/*Het is raar om dit te vergelijken, want het zijn verschillende type data.*/ 
